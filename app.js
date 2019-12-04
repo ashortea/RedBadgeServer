b5865ad054a3201344f07cd686db2368ad87ac58
ashorrtea <ashortea@gmail.com>
2019-12-04 11:05:09 -0500

    reset

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const games = require('./controllers/gamesController');//main list of games
 const favGames = require('./controllers/favGamesController');//favorites added by users
 //Database
 const sequelize = require('./db');
-sequelize.sync({force: true});
+sequelize.sync();
 app.use(require('./middleware/headers'));
 app.use(express.json());
 app.use(cors())
@@ -17,7 +17,7 @@ app.use(cors())
 //Routes/  
 app.use('/auth', user);
 app.use('/games', games) //all games
-app.use('/fav', favGames)//all favGames
+app.use('/favs', favGames)//all favGames
 app.use(require('./middleware/validate-session')); 
 //App Listen
 app.listen(process.env.PORT, () => console.log(`listening on ${process.env.PORT}`));//port is 3000
